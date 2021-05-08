f73ac0da03ab5a4fd4e6bed1b044444b70502768
yndx-pc <1543809283@qq.com>
2021-05-08 15:58:05 +0800

    test

--- src/main/java/com/example/shijian/controller/TestController.java
+++ src/main/java/com/example/shijian/controller/TestController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,13 @@ public class TestController {
 
     @RequestMapping("/test")
     @ResponseBody
+    @CrossOrigin
     public void index(HttpServletResponse response) throws JsonProcessingException {
+        String sql = "select F_name name,votes vote " +
+                "from food_frugal_foods " +
+                "where votes > 0 " +
+                "order by vote desc";
+//        sql = "select F_name name from food_frugal_foods";
         response.setContentType("application/json");
 
         response.setCharacterEncoding("utf-8");
@@ -33,7 +40,7 @@ public class TestController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from food_frugal_foods where votes is not null");
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
         printWriter.write(new ObjectMapper().writeValueAsString(list));
     }
 }
